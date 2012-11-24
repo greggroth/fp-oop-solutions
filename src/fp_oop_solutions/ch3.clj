bae81380806f61658e6deea109dc941973cfa56c
Greggory Rothmeier <gregg@highgroove.com>
2012-11-24 09:25:27 -0500

    add second part for ex 1

--- src/fp_oop_solutions/ch3.clj
+++ src/fp_oop_solutions/ch3.clj
@@ -2,8 +2,13 @@
 
 (load-file "src/fp_oop_solutions/sources/add-and-make.clj")
 
-(def add
+; Ex 3
+(def add1
   (fn [point1 point2]
     (Point (+ (x point1) (x point2))
            (+ (y point1) (y point2)))))
+
+(def add2
+  (fn [point1 point2]
+    (shift point1 (x point2) (y point2))))
 
--- test/fp_oop_solutions/ch3_test.clj
+++ test/fp_oop_solutions/ch3_test.clj
@@ -5,4 +5,5 @@
 (def point1 (Point 1 1))
 (def point2 (Point 3 5))
 
-(fact (add point1 point2) => (contains {:x 4, :y 6}))
+(fact (add1 point1 point2) => (contains {:x 4, :y 6}))
+(fact (add2 point1 point2) => (contains {:x 4, :y 6}))
